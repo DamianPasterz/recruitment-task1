020984c232776247e46cf8b0a90413dcb8a75b6f
Damian Pasterz <damian.pasterz@gmail.com>
2024-02-04 13:02:33 +0100

    add index.js and data set

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+document.addEventListener("click", (e) => {
+  const toggleDropdown = (dropdown) => {
+    dropdown.classList.toggle("active");
+    const rotation = dropdown.classList.contains("active") ? 180 : 0;
+    const svgIcon = dropdown.querySelector("svg[data-dropdown-icon]");
+    if (svgIcon) {
+      svgIcon.style.transform = `rotate(${rotation}deg)`;
+    }
+
+    const linkElement = dropdown.querySelector(".category__item > a");
+    if (linkElement) {
+      linkElement.classList.toggle("active");
+    }
+  };
+
+  const closeDropdown = (dropdown) => {
+    dropdown.classList.remove("active");
+    const svgIcon = dropdown.querySelector("svg[data-dropdown-icon]");
+    if (svgIcon) {
+      svgIcon.style.transform = "rotate(0deg)";
+    }
+
+    const linkElement = dropdown.querySelector(".category__item > a");
+    if (linkElement) {
+      linkElement.classList.remove("active");
+    }
+  };
+
+  const isDropdownButton = e.target.closest("[data-dropdown]");
+  if (!isDropdownButton) return;
+
+  const currentDropdown = e.target.closest("[data-dropdown]");
+  toggleDropdown(currentDropdown);
+
+  document
+    .querySelectorAll("[data-dropdown].active")
+    .forEach((dropdown) => {
+      if (dropdown !== currentDropdown) {
+        closeDropdown(dropdown);
+      }
+    });
+});
